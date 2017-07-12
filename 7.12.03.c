4b99bca789293d4bdc6b716e5662951e9236adb3
apktool <apktoolOo@gmail.com>
2017-07-12 08:44:08 +0800

    Inline Assmbly | Using C macros function

---
+++ 7.12.03.c
@@ -0,0 +1,32 @@
+// Inline Assmbly | Using C macros function
+
+#include <stdio.h>
+
+#define GREATER(a, b, result) ({ \
+		__asm__ __volatile__( \
+				"cmp %1, %2\n\t" \
+				"jge 0f\n\t" \
+				"movl %1, %0\n\t" \
+				"jmp 1f\n\t" \
+				"0:\n\t" \
+				"movl %2, %0\n\t" \
+				"1:" \
+				:"=r"(result) \
+				:"r"(a), "r"(b)); \
+		})
+
+int main(int argc, char* argv[]){
+
+	int data1 = 10;
+	int data2 = 20;
+	int result;
+
+	GREATER(data1, data2, result);
+	printf("a = %d, b = %d result: %d\n", data1, data2, result);
+
+	data1 = 30;
+	GREATER(data1, data2, result);
+	printf("a = %d, b = %d result: %d\n", data1, data2, result);
+
+	return 0;
+}
